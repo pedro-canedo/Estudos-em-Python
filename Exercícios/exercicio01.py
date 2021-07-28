5c038591e3f320dc1cb5db2ab3e5a33c37052579
Pedro Augusto Canedo Araujo Obalhe <82132100+pedrim775@users.noreply.github.com>
2021-07-28 16:53:19 -0300

    Create exercicio01.py
    #Programa para cadastrar pessoas e retornar se elas são maiores ou menores de idade

---
+++ Exercícios/exercicio01.py
@@ -0,0 +1,17 @@
+#Programa para cadastrar pessoas e retornar se elas são maiores ou menores de idade
+galera = list()
+dado = list()
+totmai = totmen = 0
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+for p in galera:
+    if p[1] >=21:
+        print(f'{p[0]} é maior de idade.')
+        totmai +=1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmen +=1
+print(f'Temos {totmai} maiores de idade e {totmen} menores de idade')
