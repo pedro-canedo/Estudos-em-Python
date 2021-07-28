987b4be5ef09f3b8baa10bde384549b826c41f31
Pedro Augusto Canedo Araujo Obalhe <82132100+pedrim775@users.noreply.github.com>
2021-07-28 18:11:29 -0300

    Create exercicio04.py
    #valida a entrada de somente números

---
+++ Exercícios/exercicio04.py
@@ -0,0 +1,20 @@
+#valida a entrada de somente números
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int (n)
+            ok = True
+        else:
+            print('\033[0;31mERRO ! Digite um número válido\033[m')
+        if ok:
+            break
+    return valor
+
+
+#Programa principal. //validação de dados
+
+n = leiaint('Digite um numero: ')
+print(f'Você acabou de digitar o número {n}')
