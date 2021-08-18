2cd70151009c2510a8bc8d79cb61ea5274ae38f3
Pedro Augusto Canedo Araujo Obalhe <82132100+pedrim775@users.noreply.github.com>
2021-08-18 16:10:19 -0300

    Estrutura de decisão

---
+++ Exercícios/exercicio09.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+
+if n1 > n2:
+    print(f'O maiór valor digitado foi o número:{n1}')
+
+else:
+    print(f'O maiór valor digitado foi o número:{n2}')
